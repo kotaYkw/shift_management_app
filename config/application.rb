0d8d8343e09b566290893e28820a144fd6194b01
Kota Yukawa <ykwkota@gmail.com>
2020-05-26 16:19:12 +0000

    Finish user edit, update, index, and destroy actions

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module ShiftManagementApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    # config.hosts << "76233de79a114eb9b6497255888fa037.vfs.cloud9.us-east-2.amazonaws.com"
+    # config.hosts << "localhost:3000"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
+  
+  config.hosts << "76233de79a114eb9b6497255888fa037.vfs.cloud9.us-east-2.amazonaws.com"
 end
